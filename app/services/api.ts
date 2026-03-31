b5f3a98e495d0096f51ef108cc54e2b563c69c3c
Anvesh Yadav <anveshyadavmokam@gmail.com>
2026-03-31 16:48:56 +0530

    changed-ip

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://16.112.57.1:8080';
+const API_BASE_URL = 'http://16.112.118.221:8080';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
